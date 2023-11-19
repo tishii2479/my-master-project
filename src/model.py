3a6379022e1cea10215f9de1c4d5edd823179314
Tatsuya Ishii <tishii2479@gmail.com>
2023-11-20 01:13:47 +0900

    wip

--- src/model.py
+++ src/model.py
@@ -35,3 +35,54 @@ class PositionalEncoding(torch.nn.Module):
         """
         x = x + self.pe[: x.size(1)]
         return self.dropout(x)
+
+
+class Model4(torch.nn.Module):
+    def __init__(
+        self,
+        item_size: int,
+        user_feature_dim: int,
+        num_layers: int = 4,
+        d_model: int = 64,
+        dim_feedforward: int = 128,
+        nhead: int = 4,
+    ) -> None:
+        super().__init__()
+        self.user_embedding = torch.nn.Linear(user_feature_dim, d_model)
+        self.item_embedding = torch.nn.Embedding(item_size, d_model)
+
+        self.positional_encoding = PositionalEncoding(d_model=d_model)
+        self.transformer_layer = torch.nn.TransformerEncoder(
+            encoder_layer=torch.nn.TransformerEncoderLayer(
+                d_model=d_model,
+                nhead=nhead,
+                dim_feedforward=dim_feedforward,
+                batch_first=True,
+            ),
+            num_layers=num_layers,
+            mask_check=False,
+        )
+        self.clv_layer = TwoFeedForwardLayer(d_model, d_model // 2, 1)
+        self.target_layer = TwoFeedForwardLayer(d_model, d_model * 2, d_model)
+
+    def forward(
+        self,
+        user_features: torch.Tensor,
+        item_indices: torch.Tensor,
+        target_indices: torch.Tensor,
+        padding_mask: torch.Tensor,
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        e_u = self.user_embedding.forward(user_features)
+        e_v = self.item_embedding.forward(item_indices)
+        e_v = self.positional_encoding.forward(e_v)
+        H = torch.cat((e_u.unsqueeze(1), e_v), dim=1)
+        H = self.transformer_layer.forward(H, src_key_padding_mask=padding_mask)
+
+        y_clv = torch.sigmoid(self.clv_layer.forward(H[:, 0]))
+        H_v = self.target_layer.forward(H[:, 1:])
+
+        e_target = self.item_embedding.forward(target_indices)
+        v_target = torch.matmul(H_v.unsqueeze(-2), e_target.mT)
+        y_target = torch.sigmoid(v_target)
+
+        return y_clv.squeeze(), y_target.squeeze()
--- tests/test_ml_train.py
+++ tests/test_ml_train.py
@@ -1,6 +1,6 @@
 import torch
 
-from src.model import PositionalEncoding
+from src.model import Model4, PositionalEncoding
 
 
 def test_position_encoding() -> None:
@@ -9,3 +9,71 @@ def test_position_encoding() -> None:
     pe_layer = PositionalEncoding(d_model)
     x = torch.rand(t_shape)
     assert pe_layer.forward(x).shape == t_shape
+
+
+def test_padding_mask() -> None:
+    torch.manual_seed(2)
+    d_model = 16
+    batch_size = 2
+    seq_length = 5
+    item_size = 3
+    user_feature_dim = 2
+    sample_size = 3
+    model = Model4(
+        item_size=3,
+        user_feature_dim=user_feature_dim,
+        num_layers=4,
+        d_model=d_model,
+        dim_feedforward=d_model,
+        nhead=2,
+    )
+    model.train()
+    user_features = torch.rand((batch_size, user_feature_dim))
+    item_indices = torch.randint(0, item_size, (batch_size, seq_length))
+    target_indices = torch.randint(0, item_size, (batch_size, seq_length, sample_size))
+    mask = item_indices == 1
+    padding_mask = torch.cat(
+        [
+            torch.BoolTensor([False for _ in range(batch_size)]).unsqueeze(1),
+            item_indices == 2,
+        ],
+        dim=1,
+    )
+    target_labels = torch.FloatTensor(
+        [
+            [[1] + [0] * (sample_size - 1) for _ in range(seq_length)]
+            for _ in range(batch_size)
+        ]
+    )
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+    for i in range(1000):
+        # print("user_features:", user_features)
+        # print("item_indices:", item_indices)
+        # print("target_indices:", target_indices)
+        # print("padding_mask:", padding_mask)
+        _, y_target = model.forward(
+            user_features, item_indices, target_indices, padding_mask
+        )
+        # print("y_clv:", y_clv)
+        # print("y_target:", y_target)
+        # print("target_labels:", target_labels)
+
+        # print("mask:", mask)
+
+        loss = torch.nn.functional.binary_cross_entropy(
+            y_target[mask].flatten(), target_labels[mask].flatten()
+        )
+
+        # print("y_target:", y_target[mask].flatten())
+        # print("target_labels:", target_labels[mask].flatten())
+        if i % 100 == 0:
+            print("y_target:", y_target[mask])
+            print("target_labels:", target_labels[mask])
+            print("loss:", loss.item())
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    assert False
